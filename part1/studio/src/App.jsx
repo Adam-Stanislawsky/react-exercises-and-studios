b619d5204bee86c374d82b8d4ffe72fa4b776ea0
Adam Stanislawsky <adamstanislawsky@Adams-MBP.attlocal.net>
2024-08-20 09:27:41 -0500

    completed react pt 1 studio

--- part1/studio/src/App.jsx
+++ part1/studio/src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import RecipeDescription from './components/Description'
 import RecipeIngredients from './components/Ingredients'
+import RecipePhoto from './components/Photos'
 
 function App() {
 
   return (
     <>
       <div className="App">
-        <div>
+        <div className="recipePhotoBlock">
+        <RecipePhoto />
+          <div>
           <RecipeDescription />
           <RecipeIngredients />
+          </div>
         </div>
       </div>
       
